db2eb324cee2a7805e90d8b2bd0ece5270b5aea5
Christian Bernard Bacani <bones.bacani@gmail.com>
2023-09-15 14:24:33 +0800

    Add calculator

---
+++ 25-javascript_objects/main.js
@@ -0,0 +1,11 @@
+// 8 Data Types
+//   1. String
+//   2. Boolean
+//   3. Number
+//   4. Null
+//   5. undefined
+//   6. bigint
+//   7. symbol
+//   Non-oprimitive
+//   8. array
+//   9. object
